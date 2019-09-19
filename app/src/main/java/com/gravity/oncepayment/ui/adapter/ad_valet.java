53a3a002026121058296ccba6d0ead7d38b6ebf9
mohammad mobasher <m.mobasher.z@gmail.com>
2019-09-19 20:10:02 +0430

    create rv_valet

---
+++ app/src/main/java/com/gravity/oncepayment/ui/adapter/ad_valet.java
@@ -0,0 +1,27 @@
+package com.gravity.oncepayment.ui.adapter;
+
+import android.view.View;
+import android.widget.TextView;
+
+import com.gravity.oncepayment.R;
+
+public class ad_valet  {
+
+
+
+    class CustomViewHolder {
+//            RecyclerView.ViewHolder {
+//        public TextView txt_ViewOptions, txt_BagName, txt_amount,txt_trasactionNum;
+//        public CustomViewHolder(View itemView) {
+//            super(itemView);
+//            findView();
+//        }
+//        private void findView(){
+//            txt_ViewOptions = itemView.findViewById(R.id.txt_ViewOptions);
+//            txt_BagName = itemView.findViewById(R.id.txt_BagName);
+//            txt_amount = itemView.findViewById(R.id.txt_amount);
+//            txt_trasactionNum = itemView.findViewById(R.id.txt_trasactionNum);
+//        }
+    }
+
+}
--- app/src/main/java/com/gravity/oncepayment/ui/fragment/BagFragment.java
+++ app/src/main/java/com/gravity/oncepayment/ui/fragment/BagFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -14,13 +15,25 @@ import com.gravity.oncepayment.R;
 public class BagFragment extends Fragment
 {
 
+
+
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState)
     {
 
         View view = inflater.inflate(R.layout.fragment_bag, container, false);
-
+        init(view);
         return view;
     }
+
+
+    private void init(View view){
+
+
+
+
+    }
+
 }

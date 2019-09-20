87a1bd31a39185f727eb9181bac466332dd72c23
a.r.motahhari <a.r.motahhari@gmail.com>
2019-09-20 17:40:25 +0430

    read paymentTransactionGroups from db

--- app/src/main/java/com/gravity/oncepayment/viewModel/PaymentTransactionGroupViewModel.java
+++ app/src/main/java/com/gravity/oncepayment/viewModel/PaymentTransactionGroupViewModel.java
@@ -11,17 +11,17 @@ import com.gravity.oncepayment.model.repository.Repository;
 
 import java.util.List;
 
-public class PatmentTransactionGroupViewModel extends AndroidViewModel {
+public class PaymentTransactionGroupViewModel extends AndroidViewModel {
 
-    public PatmentTransactionGroupViewModel(@NonNull Application application) {
+    public PaymentTransactionGroupViewModel(@NonNull Application application) {
         super(application);
     }
 
-    LiveData<List<PaymentTransactionGroup>> getAllDownsByDateRange(String startDate, int count) {
+    public LiveData<List<PaymentTransactionGroup>> getAllDownsByDateRange(String startDate, int count) {
         return Repository.getInstance().getAllDownsByDateRange(startDate, count);
     }
 
-    LiveData<List<PaymentTransactionGroup>> getAllUpsByDateRange(String startDate, int count) {
+    public LiveData<List<PaymentTransactionGroup>> getAllUpsByDateRange(String startDate, int count) {
         return Repository.getInstance().getAllUpsByDateRange(startDate, count);
     }
 }
